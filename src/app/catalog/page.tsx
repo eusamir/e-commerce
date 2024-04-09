bbc2ab66f10b112f458a6c6ef747c0ac32335c20
eusamir <samir.andrade@ceuma.br>
2024-04-09 00:27:09 -0300

    first commit

--- src/app/catalog/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function Catalog(){
-  await new Promise(response=>setTimeout(response,2000))
-
-  return <h1>Catalogo</h1>
-
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-//podemos deixar a função assincrona para o serverside aguardar 
+import { GithubProfile } from "@/components/github-profile";
 
-// se um componente for async posso carregar dados dentro do componente 
-//só é feito quando precisa carregar dados assim que o componente for exibido em tela(SERVERSIDE)
-// essa situação acima resolve o problema de indexação 
-
-export default async function Home() {
-  await new Promise(resolve=> setTimeout(resolve, 1000))
-
-  const response = await fetch('https://api.github.com/users/eusamir')
-  const user = await response.json()
+export default function Home() {
   return (
-    <pre>{JSON.stringify(user, null, 2)}</pre>
+    <div>
+      {/* @ts-expect-error Server Component */}
+      <GithubProfile/>
+    </div>
   )
 }
